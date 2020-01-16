168d5af30ff15d25ecd784005d9b34104d5053d6
Akinkunmi Okunola <Akinkunmi.Okunola@Interswitchng.com>
2020-01-16 17:12:45 +0100

    removed loop over sheets

--- FileUploadAndValidation/FileUploadAndValidation/FileReaderImpl/XlsxFileReader.cs
+++ FileUploadAndValidation/FileUploadAndValidation/FileReaderImpl/XlsxFileReader.cs
@@ -11,30 +11,29 @@ namespace FileUploadAndValidation.FileReaders
         {
             var rowList = new List<Row>();
 
-            using(var stream = new MemoryStream(content))
+            using (var stream = new MemoryStream(content))
             using (ExcelPackage excelPackage = new ExcelPackage(stream))
             {
                 //loop all worksheets
-                foreach (ExcelWorksheet worksheet in excelPackage.Workbook.Worksheets)
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[0];
+
+                //loop all rows
+                for (int i = worksheet.Dimension.Start.Row; i <= worksheet.Dimension.End.Row; i++)
                 {
-                    //loop all rows
-                    for (int i = worksheet.Dimension.Start.Row; i <= worksheet.Dimension.End.Row; i++)
+                    var row = new Row();
+                    row.Index = i;
+                    //loop all columns in a row
+                    for (int j = worksheet.Dimension.Start.Column; j <= worksheet.Dimension.End.Column; j++)
                     {
-                        var row = new Row();
-                        row.Index = i;
-                        //loop all columns in a row
-                        for (int j = worksheet.Dimension.Start.Column; j <= worksheet.Dimension.End.Column; j++)
+                        //add the cell data to the List
+                        if (worksheet.Cells[i, j].Value != null)
                         {
-                            //add the cell data to the List
-                            if (worksheet.Cells[i, j].Value != null)
-                            {
-                                //validate each row of the sheet
-                                row.Columns.Add(new Column() { Index = j, Value = worksheet.Cells[i, j].Value.ToString() });
-                            }
+                            row.Columns.Add(new Column() { Index = j, Value = worksheet.Cells[i, j].Value.ToString() });
                         }
-                        rowList.Add(row);
                     }
+                    rowList.Add(row);
                 }
+
             }
 
             return rowList;
