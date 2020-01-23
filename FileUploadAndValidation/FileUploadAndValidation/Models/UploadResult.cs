56020e736695c7603b5eddb3e21bf5868971187d
Akinkunmi Okunola <Akinkunmi.Okunola@Interswitchng.com>
2020-01-23 12:06:14 +0100

    plumbing work

---
+++ FileUploadAndValidation/FileUploadAndValidation/Models/UploadResult.cs
@@ -0,0 +1,35 @@
+﻿using FilleUploadCore.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileUploadAndValidation.Models
+{
+
+    public class UploadResult
+    {
+        public UploadResult()
+        {
+            ValidRows = new List<int>();
+            Failures = new List<Failure>();
+        }
+
+        public string ErrorMessage { get; set; }
+
+        public Guid ScheduleId { get; set; }
+
+        public IList<int> ValidRows { get; set; }
+
+        public IList<Failure> Failures { get; set; }
+
+        public int RowsCount { get; set; }
+
+        public class Failure
+        {
+            public int? RowNumber { get; set; }
+
+            public IList<ValidationError> ColumnValidationErrors { get; set; }
+        }
+    }
+
+}
