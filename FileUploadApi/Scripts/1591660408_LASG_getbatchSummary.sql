8363980bd6c366fbcfd6745c091b0244ec7408e7
Tunmise.Fasipe <tunmise.fasipe@interswitchgroup.com>
2020-06-09 00:58:14 +0100

    Added script to get batch summary

---
+++ FileUploadApi/Scripts/1591660408_LASG_getbatchSummary.sql
@@ -0,0 +1,111 @@
+go
+
+if OBJECT_ID('sp_get_lasg_multitax_payments_status_by_transactions_summary_id') IS NOT NULL
+begin
+    DROP PROCEDURE sp_get_lasg_multitax_payments_status_by_transactions_summary_id
+end
+go
+
+CREATE PROCEDURE sp_get_lasg_multitax_payments_status_by_transactions_summary_id
+    @transactions_summary_id bigint,
+    @page_size int,
+    @page_number int,
+    @status int,
+    @tax_type nvarchar = null
+AS
+		if(@status = 0)
+			begin
+				SELECT 
+					Id,
+					product_code,
+					item_code,
+					customer_id,
+					payer_id,
+					agency_code,
+					revenue_code,
+					start_period,
+					end_period,
+					narration,
+					amount,
+					tax_type,
+					customer_name,
+					error,
+					row_status,
+					created_date,
+					modified_date,
+					row_num,
+					transactions_summary_id,
+					initial_validation_status
+				FROM    ( SELECT   *, ROW_NUMBER() over (order by Id asc) AS RowNum
+					  FROM      tbl_lirs_multi_tax_transactions_detail
+					  WHERE     transactions_summary_id = @transactions_summary_id
+					) AS RowConstrainedResult
+				WHERE   RowNum >= ((@page_number * @page_size) - (@page_size)) + 1
+				AND RowNum < @page_number * @page_size + 1
+				ORDER BY RowNum
+			end
+		else
+			begin
+				if(@status = 1)
+					begin
+						SELECT 
+							Id,
+							product_code,
+							item_code,
+							customer_id,
+							payer_id,
+							agency_code,
+							revenue_code,
+							start_period,
+							end_period,
+							narration,
+							amount,
+							tax_type,
+							customer_name,
+							error,
+							row_status,
+							created_date,
+							modified_date,
+							row_num,
+							transactions_summary_id,
+							initial_validation_status
+						FROM    ( SELECT   *, ROW_NUMBER() over (order by Id asc) AS RowNum
+							  FROM      tbl_lirs_multi_tax_transactions_detail
+							  WHERE     transactions_summary_id = @transactions_summary_id and row_status = 'Valid'
+							) AS RowConstrainedResult
+						WHERE   RowNum >= ((@page_number * @page_size) - (@page_size)) + 1
+						AND RowNum < @page_number * @page_size + 1
+						ORDER BY RowNum
+					end
+				else
+					begin
+						SELECT 
+							Id,
+							product_code,
+							item_code,
+							customer_id,
+							payer_id,
+							agency_code,
+							revenue_code,
+							start_period,
+							end_period,
+							narration,
+							amount,
+							tax_type,
+							customer_name,
+							error,
+							row_status,
+							created_date,
+							modified_date,
+							row_num,
+							transactions_summary_id,
+							initial_validation_status
+						FROM    ( SELECT   *, ROW_NUMBER() over (order by Id asc) AS RowNum
+							  FROM      tbl_lirs_multi_tax_transactions_detail
+							  WHERE     transactions_summary_id = @transactions_summary_id and row_status = 'Invalid'
+							) AS RowConstrainedResult
+						WHERE   RowNum >= ((@page_number * @page_size) - (@page_size)) + 1
+						AND RowNum < @page_number * @page_size + 1
+						ORDER BY RowNum
+					end
+	end
