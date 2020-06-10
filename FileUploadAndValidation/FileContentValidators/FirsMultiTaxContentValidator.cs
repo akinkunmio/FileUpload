23dd452c06a5a9ac8ba4f0bd9b04abdd7fdaa92d
Akinkunmi Okunola <akinkunmi.okunola@interswitchng.com>
2020-06-10 18:11:03 +0100

    added changes for check for empty tax type

--- FileUploadAndValidation/FileContentValidators/FirsMultiTaxContentValidator.cs
+++ FileUploadAndValidation/FileContentValidators/FirsMultiTaxContentValidator.cs
@@ -48,14 +48,50 @@ namespace FileUploadAndValidation.FileContentValidators
             var rowDetail = new RowDetail();
             var result = new ValidateRowModel();
 
-            string rowTaxType = default;
+            string rowTaxType = "";
 
             if(authority.ToLower().Equals(GenericConstants.Firs))
                 rowTaxType = row.Columns[15].Value;
 
+            if (string.IsNullOrWhiteSpace(rowTaxType))
+                return new ValidateRowModel
+                {
+                    isValid = false,
+                    Failure = new Failure
+                    {
+                        ColumnValidationErrors = new List<ValidationError> {
+                             new ValidationError {
+                                ErrorMessage = $"Field should not be empty",
+                                PropertyName = ContentTypeColumnContract.FirsMultiTaxWht()[15].ColumnName
+                             }
+                         },
+                        Row = new RowDetail
+                        {
+                            RowNum = row.Index,
+                            BeneficiaryTin = row.Columns[0].Value,
+                            BeneficiaryName = row.Columns[1].Value,
+                            BeneficiaryAddress = row.Columns[2].Value,
+                            ContractDate = row.Columns[3].Value,
+                            ContractDescription = row.Columns[4].Value,
+                            ContractAmount = row.Columns[5].Value,
+                            ContractType = row.Columns[6].Value,
+                            PeriodCovered = row.Columns[7].Value,
+                            InvoiceNumber = row.Columns[8].Value,
+                            WhtRate = row.Columns[9].Value,
+                            WhtAmount = row.Columns[10].Value,
+                            Amount = row.Columns[11].Value,
+                            Comment = row.Columns[12].Value,
+                            DocumentNumber = row.Columns[13].Value,
+                            PayerTin = row.Columns[14].Value,
+                            TaxType = row.Columns[15].Value
+                        }
+                    }
+
+                };
+                
             var columnContracts = GetColumnContractByTaxType(authority, rowTaxType);
               
-            var validationResult = GenericHelpers.ValidateRowCell(row, columnContracts);
+            ValidateRowResult validationResult = GenericHelpers.ValidateRowCell(row, columnContracts);
 
             if (rowTaxType.ToLower().Equals(GenericConstants.Wht))
                 rowDetail = new RowDetail
